8ab9a3d7f9ad7d8d5c052e8064fe2aed36ed0c27
Luis Ramirez <sdaewa22@gmail.com>
2021-04-29 16:46:46 +0200

    secured subprocess call

--- script.py
+++ script.py
@@ -7,7 +7,6 @@ new_mac = input('new MAC address > ')
 
 print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
 
-
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
