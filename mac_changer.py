3ea72f5629ace4be1ccc5fb63fb6a1e8a3d9320c
Luis Ramirez <sdaewa22@gmail.com>
2021-04-29 21:08:49 +0200

    get arguments fx

--- mac_changer.py
+++ mac_changer.py
@@ -8,7 +8,7 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    (options, arguments) = parser.parse_args()
+    return parser.parse_args()
 
 
 def change_mac(interface, new_mac):
@@ -18,4 +18,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+(options, arguments) = get_arguments()
 change_mac(options.interface, options.new_mac)
