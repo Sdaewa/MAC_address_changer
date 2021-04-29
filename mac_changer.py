7f5980a08a860ba0c1c621c6cba88a3fd2d8441a
Luis Ramirez <sdaewa22@gmail.com>
2021-04-29 20:25:11 +0200

    user input using cli args

--- mac_changer.py
+++ mac_changer.py
@@ -9,15 +9,15 @@ parser = optparse.OptionParser()
 parser.add_option('-i', '--interface', dest='interface',
                   help='Interface to change MAC address')
 
-parser.add_option('-m', '--interface', dest='interface',
-                  help='Interface to change MAC address')
+parser.add_option('-m', '--mac', dest='new_mac',
+                  help='New MAC address')
 
 
-parser.parse_args()
+(options, arguments) = parser.parse_args()
 
 
-interface = input('interface > ')
-new_mac = input('new MAC address > ')
+interface = options.interface
+new_mac = options.new_mac
 
 
 print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
