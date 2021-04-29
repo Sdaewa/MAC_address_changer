ce702107a05c09a9c9d7e7d669cdbb1b22b317d8
Luis Ramirez <sdaewa22@gmail.com>
2021-04-29 21:07:22 +0200

    fx

--- mac_changer.py
+++ mac_changer.py
@@ -3,25 +3,19 @@ import subprocess
 import optparse
 
 
-parser = optparse.OptionParser()
-
 # options
-parser.add_option('-i', '--interface', dest='interface',
-                  help='Interface to change MAC address')
-
-parser.add_option('-m', '--mac', dest='new_mac',
-                  help='New MAC address')
-
-
-(options, arguments) = parser.parse_args()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
 
 
-interface = options.interface
-new_mac = options.new_mac
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
 
-print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
-
-subprocess.call(['ifconfig', interface, 'down'])
-subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
-subprocess.call(['ifconfig', interface, 'up'])
+change_mac(options.interface, options.new_mac)
