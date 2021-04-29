59ecb2c0e50df17c8ae2f2deb2de7f82052c79f5
Luis Ramirez <sdaewa22@gmail.com>
2021-04-29 16:09:39 +0200

    basic script

--- script.py
+++ script.py
@@ -2,4 +2,7 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig wlan0 down", shell=True)
+# change < 00: 11: 33: 44: 55: 66 > for new address
+subprocess.call("ifconfig wlan0 hw ether 00:11:33:44:55:66", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
